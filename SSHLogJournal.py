3481876fff036a504ee03149fa6b2b8d7f3439be
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-04-18 00:03:26 +0200

    Zad 7

---
+++ SSHLogJournal.py
@@ -0,0 +1,42 @@
+from datetime import datetime
+from ipaddress import IPv4Address
+
+from SSHLogEntry import classify_entry
+
+
+class SSHLogJournal:
+    def __init__(self):
+        self.entries = []
+
+    def __len__(self):
+        return len(self.entries)
+
+    def __iter__(self):
+        return iter(self.entries)
+
+    def __contains__(self, item):
+        return item in self.entries
+
+    def __getitem__(self, key):
+        if isinstance(key, slice):
+            return self.entries[key]
+        elif isinstance(key, int):
+            return self.entries[key]
+        elif isinstance(key, str):
+            return self.get_logs_by_ip(key)
+        elif isinstance(key, tuple) and len(key) == 2 and all(isinstance(item, datetime) for item in key):
+            start_date, end_date = key
+            return self.get_logs_by_daterange(start_date, end_date)
+        else:
+            raise TypeError("Unsupported key type")
+
+    def append(self, entry_text):
+        entry = classify_entry(entry_text)
+        if entry and entry.validate():
+            self.entries.append(entry)
+
+    def get_logs_by_ip(self, ip_address):
+        return [entry for entry in self.entries if entry.IPv4_address == IPv4Address(ip_address)]
+
+    def get_logs_by_daterange(self, start_date, end_date):
+        return [entry for entry in self.entries if start_date <= entry.time <= end_date]
--- tester.py
+++ tester.py
@@ -1,4 +1,7 @@
+import datetime
+
 import SSHLogEntry
+import SSHLogJournal
 
 entry = SSHLogEntry.classify_entry(
     "Dec 10 09:11:22 LabSZ sshd[24439]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]")
@@ -8,3 +11,23 @@ print(entry.IPv4_address)
 print(entry.validate())
 print(entry.has_ip)
 print(entry.__repr__())
+
+print("\n--------------------------------------------------\n")
+
+journal = SSHLogJournal.SSHLogJournal()
+journal.append("Dec 10 06:55:46 LabSZ sshd[24200]: reverse mapping checking getaddrinfo for ns.marryaldkfaczcz.com [173.234.31.186] failed - POSSIBLE BREAK-IN ATTEMPT!")
+journal.append("Dec 10 09:11:22 LabSZ sshd[24439]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]")
+journal.append("Dec 10 11:04:43 LabSZ sshd[25544]: pam_unix(sshd:auth): authentication failure; logname= uid=0 euid=0 tty=ssh ruser= rhost=183.62.140.253  user=root")
+
+print(len(journal))
+for entry in journal:
+    print(entry)
+
+print(journal[:1])
+
+print(entry in journal)
+print(journal["103.99.0.122"])
+
+start_date = datetime.datetime(year=1900, month=12, day=10, hour=6, minute=55, second=43)
+end_date = datetime.datetime(year=1900, month=12, day=10, hour=10, minute=4, second=43)
+print(journal[(start_date, end_date)])
