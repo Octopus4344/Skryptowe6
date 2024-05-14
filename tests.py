687ab6ed6f3f5fd94d3ea99dc278eea827b7b045
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-05-14 09:35:48 +0200

    Add test for log type

--- tests.py
+++ tests.py
@@ -43,3 +43,24 @@ def test_extract_time(log_content, expected_time):
 def test_IPv4_extraction(log_content, expected_ip):
     entry = classify_entry(log_content)
     assert entry.IPv4_address == expected_ip
+
+
+sample_logs_2 = [
+    "Dec 10 06:55:48 LabSZ sshd[24200]: Failed password for invalid user webmaster from1 73.234.31.186 port 38926 ssh2",
+    "Dec 10 09:32:20 LabSZ sshd[24680]: Accepted password for fztu from 119.137.62.142 port 49116 ssh2",
+    "Dec 10 11:03:40 LabSZ sshd[25448]: error: Received disconnect from 103.99.0.122: 14: No more user authentication methods available. [preauth]",
+    "Dec 10 06:55:46 LabSZ sshd[24200]: Invalid user webmaster from 173.234.31.186",
+]
+
+
+@pytest.mark.parametrize(
+    "log_content, expected_type", [
+        (sample_logs_2[0], "Invalid Password Log Entry"),
+        (sample_logs_2[1], "Password Accepted Log Entry"),
+        (sample_logs_2[2], "Error Log Entry"),
+        (sample_logs_2[3], "Other Log Entry")
+    ]
+)
+def test_append_type(log_content, expected_type):
+    entry = classify_entry(log_content)
+    assert entry.type == expected_type
