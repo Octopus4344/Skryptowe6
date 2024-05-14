09e3fc55b03a6bdf6c449dd049dc674037798522
Jakub Czajkowski <272709@student.pwr.edu.pl>
2024-05-14 09:48:51 +0200

    Add types for SSHUser.py

--- SSHUser.py
+++ SSHUser.py
@@ -3,14 +3,13 @@ import re
 
 
 class SSHUser:
-    def __init__(self, username: str, last_login_date: datetime.datetime):
+    def __init__(self, username: str, last_login_date: datetime.datetime) -> None:
         self.username = username
         self.last_login_date = last_login_date
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{self.__class__.__name__}(username={self.username}, last_login_date={self.last_login_date})"
 
-    def validate(self):
-        pattern = r'^[a-z_][a-z0-9_-]{0,31}$'
-
+    def validate(self) -> bool:
+        pattern: str = r'^[a-z_][a-z0-9_-]{0,31}$'
         return re.match(pattern, self.username) is not None
